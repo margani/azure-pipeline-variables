3c278cf323b36863422ae99d21c64b4659611a02
Hossein Margani <hossein@margani.dev>
2022-03-03 08:34:23 +0000

    Add environment and initialize pipeline templates

---
+++ global/scripts/initialize-pipeline.ps1
@@ -0,0 +1,33 @@
+﻿[CmdletBinding()]
+param (
+    [Parameter()][String]$EnvSubscriptions
+)
+
+Function Initialize-Pipeline {
+    [CmdletBinding()]
+    param (
+        [Parameter()][String]$EnvSubscriptions
+    )
+    Process {
+        Write-Host "EnvSubscriptions: $EnvSubscriptions"
+
+        $EnvSubscriptions.Split(';', [System.StringSplitOptions]::RemoveEmptyEntries)
+        | ForEach-Object {
+            $envSubscription = $_
+            $keyValue = $envSubscription.Split(':', [System.StringSplitOptions]::RemoveEmptyEntries)
+            if ($keyValue.Length -eq 2) {
+                $envs = $keyValue[0]
+                $subscription = $keyValue[1].Trim()
+
+                $envs.Split(',', [System.StringSplitOptions]::RemoveEmptyEntries)
+                | ForEach-Object {
+                    $env = $_.Trim()
+                    Write-Output "##vso[task.setvariable variable=$env-subscription]$subscription"
+                }
+            }
+        }
+    }
+}
+
+Initialize-Pipeline `
+    -EnvSubscriptions $EnvSubscriptions
